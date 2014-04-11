8286b280f01500d8d1636a3dc2fb59b995b71d4a
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-04-11 14:10:47 +0100

    first commit

--- defaultPrefix/ViewController.m
+++ defaultPrefix/ViewController.m
@@ -6,9 +6,15 @@
 //  Copyright (c) 2014 Gabriel Massana. All rights reserved.
 //
 
+//http://stackoverflow.com/questions/23012420
+
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+
+@property (strong, nonatomic)  NSString *defaultPrefix;
 
 @end
 
@@ -18,6 +24,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(defaultPrefix:) name:UITextFieldTextDidChangeNotification object:self.textField];
+    
+    self.defaultPrefix = @"DEFAULT";
+    self.textField.text = self.defaultPrefix;
+}
+
+- (void) defaultPrefix: (NSNotification*) notification
+{
+    UITextField *notificationTextField = [notification object];
+    
+    if (notificationTextField == self.textField)
+    {
+        if ([self.textField.text length] <= [self.defaultPrefix length])
+        {
+            self.textField.text = self.defaultPrefix;
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning
