58265f3fc23ec33597c6640b1157ba133958f688
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-04-11 14:01:44 +0100

    Initial Commit

---
+++ defaultPrefix/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  defaultPrefix
+//
+//  Created by Gabriel Massana on 11/04/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ defaultPrefix/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  defaultPrefix
+//
+//  Created by Gabriel Massana on 11/04/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
